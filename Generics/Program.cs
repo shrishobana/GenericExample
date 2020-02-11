47bd6d28809d04759d120ac885547781f6f840b9
shrishobana <shrishobana@skystudent.onmicrosoft.com>
2020-02-11 19:34:27 +0100

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class MyClass<T>
+    {
+        public bool compare(T a,T b)
+        {
+            if(a.Equals(b))
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<string> my = new MyClass<string>();
+            bool result = my.compare("Hi","Hi");
+            Console.Write(result);
+           
+            MyClass<int> my1= new MyClass<int>();
+            bool result1 = my1.compare(10, 5);
+            Console.Write(result1);
+            Console.ReadKey();
+
+        }
+    }
+}
